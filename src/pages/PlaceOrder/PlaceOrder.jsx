94ef9add356acbf4710e4cd57237a96d438924b7
Archipelago <118038000+archi-pelago@users.noreply.github.com>
2024-07-22 08:21:14 +0000

    Changed Cart Amount

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -41,7 +41,7 @@ const PlaceOrder = () => {
           <hr />
           <div className="cart-total-details">
             <b>Total</b>
-            <b>${getTotalCartAmount() + 2}</b>
+            <b>${getTotalCartAmount()===0 ? 0 : getTotalCartAmount+2}</b>
           </div>
           <button id='payment'>Advance to the Payment Stage</button>
         </div>
